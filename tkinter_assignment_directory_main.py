53c1d7af12c8910423f0c13d5a50932ec2ce3e49
Kevin Elliott McCrea <kleiben@gmail.com>
2020-04-14 14:03:20 -0700

    Create tkinter_assignment_directory_main.py

---
+++ tkinter_assignment_directory_main.py
@@ -0,0 +1,64 @@
+
+
+from tkinter import *
+import tkinter as tk
+
+
+import tkinter_assignment_directory_gui
+
+
+class ParentWindow(Frame):
+    def __init__(self, master, *args, **kwargs):
+        Frame.__init__(self, master, *args, **kwargs)
+
+        # define our master frame configuration
+        self.master = master
+        self.master.minsize(520,250) #(Width, Height)
+        self.master.maxsize(520,250)
+        self.master.title("Check files")
+        self.master.configure(bg="#F0F0F0")
+
+        # load in the GUI widgets from a separate module,
+        tkinter_assignment_directory_gui.load_gui(self)
+        
+
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    App = ParentWindow(root)
+    root.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
